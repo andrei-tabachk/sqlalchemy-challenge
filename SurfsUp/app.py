be850a6d19e0be7f949f439bd6bf4b70af3e455b
andrei-tabachk <andrei.tabachk@gmail.com>
2023-05-08 20:08:16 -0400

    part 2 progressed

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -32,3 +32,45 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+@app.route ("/")
+def welcome():
+    return (
+        f"Welcome to the Andrei's Surfs Up API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/insert(yyyy-mm-dd)<start><br/>"
+        f"/api/v1.0/insert(yyyy-mm-dd)<start>/(yyyy-mm-dd)<end>"
+    )
+
+@app.route ("/api/v1.0/percipitation")
+def percipitation():
+    session = Session(engine)
+    results_perc = session.query(measurement.date,measurement.prcp).\
+                        filter(measurement.date >= '2016-08-23').\
+                        order_by(measurement.date).all()
+    session.close()
+
+    all_perc = []
+    for date, prcp in results_percipitation:
+        percip_dict = {}
+        percip_dict["date"] = date
+        percip_dict["prcp"] = prcp
+        all_perc.append(precip_dict)
+
+    return jsonify(all_perc)
+
+@app.route ("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    
+    station_results = session.query(station.station).all()
+
+    session.close()
+
+    all_station = list(np.ravel(station_results))
+
+    return jsonify(all_station)
+
